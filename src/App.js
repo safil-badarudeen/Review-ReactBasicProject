5ec3a7dedfcff15254b5b12244bfd7a4e2464cc8
safil badarudeen <safilbadarudeen@gmail.com>
2022-11-30 16:54:04 +0530

    second commmit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Review from './Review';
+
+function App() {
+  return <>
+   <div className="title">
+    <h2>Our Review</h2>
+    <div className="underline"></div>
+    <div>
+      <Review/>
+    </div>
+
+   </div>
+  
+  </>
+}
+
+export default App;
